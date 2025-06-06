31415b557fa702633c604fe16d3dc3cbf7f65df8
TivaN <naharamimo@gmail.com>
2025-06-06 10:23:26 +0300

    v0.6

---
+++ upload/catalog/language/uk-ua/product/luxshop_latestpage.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title'] 		= 'Новинки';
+
+// Text
+$_['text_empty'] 			= 'Немає новинок.';
+$_['text_quantity'] 		= 'Кількість:';
+$_['text_manufacturer'] = 'Виробник:';
+$_['text_model'] 			= 'Код товару:';
+$_['text_points'] 		= 'Бонусні бали:';
+$_['text_price'] 			= 'Ціна:';
+$_['text_tax'] 			= 'Без податку:';
+$_['text_compare'] 		= 'Порівняння товарів (%s)';
+$_['text_sort'] 			= 'Сортувати:';
+$_['text_default'] 		= 'За замовчуванням';
+$_['text_name_asc']     = 'Назва (А - Я)';
+$_['text_name_desc']    = 'Назва (Я - А)';
+$_['text_price_asc'] 	= 'За ціною (зростанням)';
+$_['text_price_desc'] 	= 'За ціною (зменшенням)';
+$_['text_rating_asc'] 	= 'За рейтингом (зростанням)';
+$_['text_rating_desc'] 	= 'За рейтингом (зменшенням)';
+$_['text_model_asc'] 	= 'За моделлю (A - Я)';
+$_['text_model_desc'] 	= 'За моделлю (Я - A)';
+$_['text_limit'] 			= 'Показувати:';
